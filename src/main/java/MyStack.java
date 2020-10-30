483c0fc1fd4495f9b76cb671a55090803ff2e905
Kamini Patil <kamini.patil@ad.infosys.com>
2020-10-30 22:14:17 +0530

    ADDED : STACK pop operation still empty and test by test cases

--- src/main/java/MyStack.java
+++ src/main/java/MyStack.java
@@ -9,12 +9,32 @@ public class MyStack<T> {
     public void push(INode<T> element) {
         myLinkedList.add(element);
     }
-    public INode<T> peak()
-    {
+
+    public boolean isEmpty() {
+        return myLinkedList.head == null;
+    }
+
+    public INode<T> peak() {
+        if (!isEmpty()) {
+            return myLinkedList.head;
+        } else {
+            System.out.println("Stack is empty");
+
+        }
+        return null;
+    }
+
+    public INode<T> pop() {
+        INode<Integer> peak = (INode<Integer>) peak();
+        while (peak != null) {
+            myLinkedList.pop();
+            peak = (INode<Integer>) peak();
+        }
         return myLinkedList.head;
     }
-    public void printStack()
-    {
+
+
+    public void printStack() {
         myLinkedList.printMyNodes();
     }
 }
--- src/test/java/StackTestCases.java
+++ src/test/java/StackTestCases.java
@@ -11,8 +11,23 @@ public class StackTestCases {
         myStack.push(myFirstNode);
         myStack.push(mySecondNode);
         myStack.push(myThirdNode);
-        INode<Integer> peak=myStack.peak();
+        INode<Integer> peak = myStack.peak();
         myStack.printStack();
-        Assert.assertEquals(myThirdNode,peak);
+        Assert.assertEquals(myThirdNode, peak);
     }
+
+    @Test
+    public void given3Numbers_WhenPopFromStack_ShouldPassResult() {
+        MyStack<Integer> myStack = new MyStack<>();
+        MyNode<Integer> myFirstNode = new MyNode<>(70);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(56);
+        myStack.push(myFirstNode);
+        myStack.push(mySecondNode);
+        myStack.push(myThirdNode);
+        myStack.printStack();
+        INode<Integer> pop = myStack.pop();
+        Assert.assertEquals(null, pop);
+    }
+
 }
