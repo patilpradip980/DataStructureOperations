5584e7fdc60cf63b8f7105d6c8876872e852f506
Kamini Patil <kamini.patil@ad.infosys.com>
2020-10-30 22:41:33 +0530

    CHANGED : Test case method name changed

--- src/test/java/QueueTestCases.java
+++ src/test/java/QueueTestCases.java
@@ -3,7 +3,7 @@ import org.junit.Test;
 
 public class QueueTestCases {
     @Test
-    public void given3Numbers_WhenAddedToStack_ShouldPassResult() {
+    public void given3Numbers_WhenAddedToQueue_ShouldPassResult() {
         MyQueue<Integer> myQueue = new MyQueue<>();
         MyNode<Integer> myFirstNode = new MyNode<>(56);
         MyNode<Integer> mySecondNode = new MyNode<>(30);
@@ -13,6 +13,6 @@ public class QueueTestCases {
         myQueue.enqueue(myThirdNode);
         myQueue.printStack();
         INode peak = myQueue.peak();
-        Assert.assertEquals(myFirstNode,peak);
+        Assert.assertEquals(myFirstNode, peak);
     }
 }
