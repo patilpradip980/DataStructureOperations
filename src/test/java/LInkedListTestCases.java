37b93463db3fffe3d1a1685e9081bc6c8d424746
Kamini Patil <kamini.patil@ad.infosys.com>
2020-10-30 18:29:25 +0530

    ADDED : Append method added and test cases passed to get 56 first element

--- src/test/java/LInkedListTestCases.java
+++ src/test/java/LInkedListTestCases.java
@@ -15,7 +15,7 @@ public class LInkedListTestCases {
     }
 
     @Test
-    public void given3Numbers_WhenAddedToLinkedList_ShouldBeAddAtTop() {
+    public void given3Numbers_When70AddedToLinkedList_ShouldBeAddAtTop() {
         MyNode<Integer> myFirstNode = new MyNode<>(70);
         MyNode<Integer> mySecondNode = new MyNode<>(30);
         MyNode<Integer> myThirdNode = new MyNode<>(56);
@@ -31,4 +31,20 @@ public class LInkedListTestCases {
 
     }
 
+    @Test
+    public void given3Numbers_When56AddedToLinkedList_ShouldBeAddAtTop() {
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.add(myFirstNode);
+        myLinkedList.append(mySecondNode);
+        myLinkedList.append(myThirdNode);
+        myLinkedList.printMyNodes();
+        boolean result = myLinkedList.head.equals(myFirstNode) &&
+                myLinkedList.head.getNext().equals(mySecondNode) &&
+                myLinkedList.tail.equals(myThirdNode);
+
+        Assert.assertTrue(result);
+    }
 }
