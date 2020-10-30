5f9e677db6bd677cb6f002501b12661eccaf1f5c
Kamini Patil <kamini.patil@ad.infosys.com>
2020-10-30 18:18:10 +0530

    ADDED : add method added to added element in list and test cases passed to verify 70 element at first

--- src/main/java/MyNode.java
+++ src/main/java/MyNode.java
@@ -1,17 +1,28 @@
-public class MyNode<K> {
+public class MyNode<K> implements INode<K> {
+
+    private INode next;
     private K key;
-    private MyNode next;
 
     public MyNode(K key) {
         this.key = key;
         this.next = null;
     }
 
-    public MyNode getNext() {
+    @Override
+    public K getKey() {
+        return key;
+    }
+
+    @Override
+    public void setKey(K key) {
+        this.key = key;
+    }
+
+    public INode<K> getNext() {
         return next;
     }
 
-    public void setNext(MyNode next) {
-        this.next = next;
+    public void setNext(INode next) {
+        this.next = (MyNode<K>) next;
     }
 }
--- src/test/java/LInkedListTestCases.java
+++ src/test/java/LInkedListTestCases.java
@@ -12,6 +12,23 @@ public class LInkedListTestCases {
         boolean result = myFirstNode.getNext().equals(mySecondNode) &&
                 mySecondNode.getNext().equals(myThirdNode);
         Assert.assertTrue(result);
+    }
+
+    @Test
+    public void given3Numbers_WhenAddedToLinkedList_ShouldBeAddAtTop() {
+        MyNode<Integer> myFirstNode = new MyNode<>(70);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(56);
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.add(myFirstNode);
+        myLinkedList.add(mySecondNode);
+        myLinkedList.add(myThirdNode);
+        myLinkedList.printMyNodes();
+        boolean result = myLinkedList.head.equals(myThirdNode) &&
+                myLinkedList.head.getNext().equals(mySecondNode) &&
+                myLinkedList.tail.equals(myFirstNode);
+        Assert.assertTrue(result);
 
     }
+
 }
