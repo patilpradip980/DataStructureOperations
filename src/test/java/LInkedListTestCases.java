8b9d5368c71769d87f24689dc530f8727d8a8737
Kamini Patil <kamini.patil@ad.infosys.com>
2020-10-30 18:04:32 +0530

    STARTED : Test cases added to to create simple linked list

---
+++ src/test/java/LInkedListTestCases.java
@@ -0,0 +1,17 @@
+import org.junit.*;
+
+public class LInkedListTestCases {
+
+    @Test
+    public void given3Numbers_WhenLinked_ShouldPassedLinkedList() {
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+        myFirstNode.setNext(mySecondNode);
+        mySecondNode.setNext(myThirdNode);
+        boolean result = myFirstNode.getNext().equals(mySecondNode) &&
+                mySecondNode.getNext().equals(myThirdNode);
+        Assert.assertTrue(result);
+
+    }
+}
